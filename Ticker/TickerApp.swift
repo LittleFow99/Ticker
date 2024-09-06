e98fce5625740346bd3e663f5872dfd17be09253
Thomas Paolino <tpao24@Outlook.com>
2024-09-05 20:29:48 -0400

    Initial Commit

---
+++ Ticker/TickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickerApp.swift
+//  Ticker
+//
+//  Created by Thomas Paolino on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
